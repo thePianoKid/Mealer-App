9bdba65e073943916758825cb45e5870aadd4265
thePianoKid <gbraden.dev@gmail.com>
2022-11-18 20:01:52 -0500

    Added login functionality for cooks

--- app/src/main/java/ca/uottawa/mealerapp/LoginPage.java
+++ app/src/main/java/ca/uottawa/mealerapp/LoginPage.java
@@ -1,20 +1,71 @@
 package ca.uottawa.mealerapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ca.uottawa.mealerapp.userclasses.User;
+import ca.uottawa.mealerapp.userclasses.UsernameConversion;
 
 public class LoginPage extends AppCompatActivity {
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView register_text = findViewById(R.id.register_text);
-        register_text.setOnClickListener(view ->
+        TextView registerText = findViewById(R.id.register_text);
+        Button signInButton = findViewById(R.id.signin_button);
+        EditText usernameInput = findViewById(R.id.login_username);
+        EditText passwordInput = findViewById(R.id.login_password);
+
+        registerText.setOnClickListener(view ->
                 startActivity(new Intent(LoginPage.this, RegisterStage1.class)));
+
+        signInButton.setOnClickListener(view -> {
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    try {
+                        Log.i(TAG, UsernameConversion.encode(username));
+                        String correctPassword = snapshot.child("cooks").child(UsernameConversion
+                                .encode(username)).child("password").getValue().toString();
+
+                        if (correctPassword.equals(password))
+                            startActivity(new Intent(LoginPage.this,
+                                    MealDisplay.class));
+                        else
+                            Toast.makeText(LoginPage.this, "Incorrect password",
+                                    Toast.LENGTH_LONG).show();
+
+                    } catch (NullPointerException e) {
+                        Toast.makeText(LoginPage.this, "User not found.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {}
+            });
+        });
     }
 }

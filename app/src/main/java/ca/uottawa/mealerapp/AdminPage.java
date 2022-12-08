fbd4c8b79cbce359d8123a300275b4d2410ed907
thePianoKid <gbraden.dev@gmail.com>
2022-12-07 21:52:18 -0500

    Updated admin page

--- app/src/main/java/ca/uottawa/mealerapp/AdminPage.java
+++ app/src/main/java/ca/uottawa/mealerapp/AdminPage.java
@@ -1,8 +1,22 @@
 package ca.uottawa.mealerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class AdminPage extends AppCompatActivity {
 
@@ -10,5 +24,39 @@ public class AdminPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference()
+                .child("complaints");
+
+        ListView listView = findViewById(R.id.complaints_list_view);
+
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            String mealName;
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mealName = list.get(i);
+                Intent intent = new Intent(AdminPage.this, ComplaintPage.class);
+                intent.putExtra("complaintKey", list.get(i));
+                startActivity(intent);
+            }
+        });
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    list.add(dataSnapshot.getKey());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
     }
 }

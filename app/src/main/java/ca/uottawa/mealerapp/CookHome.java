351b68f881feba9bbc67c654d0dc20a47b719775
thePianoKid <gbraden.dev@gmail.com>
2022-12-07 23:10:34 -0500

    Added cook home activity

---
+++ app/src/main/java/ca/uottawa/mealerapp/CookHome.java
@@ -0,0 +1,14 @@
+package ca.uottawa.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CookHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_home);
+    }
+}
